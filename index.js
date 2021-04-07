ff3bc4f63b7ad72d39ff6e2a4b38d0928af7100a
Peter Murray <681306+peter-murray@users.noreply.github.com>
2021-04-07 19:13:57 +0000

    Capturing errors from missing parameters

--- index.js
+++ index.js
@@ -3,36 +3,38 @@ const core = require('@actions/core')
   ;
 
 async function run() {
-  const privateKey = getRequiredInputValue('application_private_key')
-    , applicationId = getRequiredInputValue('application_id')
-    ;
-
   let app;
+
   try {
+    const privateKey = getRequiredInputValue('application_private_key')
+      , applicationId = getRequiredInputValue('application_id')
+      ;
     app = await githubApplication.create(privateKey, applicationId);
   } catch(err) {
     fail(err, 'Failed to initialize GitHub Application connection using provided id and private key');
   }
 
-  try {
-    const repository = process.env['GITHUB_REPOSITORY']
-      , repoParts = repository.split('/')
-    ;
-
-    const installation = await app.getRepositoryInstallation(repoParts[0], repoParts[1]);
-    if (installation && installation.id) {
-      const accessToken = await app.getInstallationAccessToken(installation.id);
-
-      // Register the secret to mask it in the output
-      core.setSecret(accessToken.token);
-      core.setOutput('token', accessToken.token);
-      core.info(JSON.stringify(accessToken));
-      core.info(`Successfully generated an access token for application.`)
-    } else {
-      fail(null, `GitHub Application is not installed on repository: ${repository}`);
+  if (app) {
+    try {
+      const repository = process.env['GITHUB_REPOSITORY']
+        , repoParts = repository.split('/')
+      ;
+  
+      const installation = await app.getRepositoryInstallation(repoParts[0], repoParts[1]);
+      if (installation && installation.id) {
+        const accessToken = await app.getInstallationAccessToken(installation.id);
+  
+        // Register the secret to mask it in the output
+        core.setSecret(accessToken.token);
+        core.setOutput('token', accessToken.token);
+        core.info(JSON.stringify(accessToken));
+        core.info(`Successfully generated an access token for application.`)
+      } else {
+        fail(null, `GitHub Application is not installed on repository: ${repository}`);
+      }
+    } catch (err) {
+      fail(err);
     }
-  } catch (err) {
-    fail(err);
   }
 }
 run();
